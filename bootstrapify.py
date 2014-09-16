b1d406a38af0f4cae7c9a8f5302f8a0491893a1c
winlu <derwinlu+git@gmail.com>
2014-09-16 12:34:03 +0200

    cleanup code, write readme

--- bootstrapify.py
+++ bootstrapify.py
@@ -1,21 +1,22 @@
 '''
 bootstrapify
 ===================================
-This plugin modifies article and page html to use bootstrap classes, i.e tables (and later more)
+This pelican plugin modifies article and page html to use bootstrap's default classes. This is especially handy if you want to write tables in markdown, since the attr_list extension does not play nice with tables
 '''
 
 from pelican import signals, contents
 from bs4 import BeautifulSoup
 import re
 
+def replace(searchterm, soup, attributes):
+    for item in soup.findAll(searchterm):
+        item.attrs['class'] = list(set(item.attrs.get('class', []) + attributes))
+
 def replace_tables(soup, attributes=['table',' table-striped', 'table-hover']):
-    for tbl in soup.findAll('table'):
-        tbl.attrs['class'] = list(set(tbl.attrs.get('class', []) + attributes))
-        print(repr(tbl.attrs.get('class')))
+    replace('table', soup, attributes)
 
 def replace_images(soup, attributes=['img-responsive']):
-    for img in soup.findAll('img'):
-        img.attrs['class'] = list(set(img.attrs.get('class', []) + attributes))
+    replace('img', soup, attributes)
 
 
 def bootstrapify(content):
@@ -26,8 +27,7 @@ def bootstrapify(content):
     replace_tables(soup)
     replace_images(soup)
 
-
-    content._content = soup.decode()#prettify?
+    content._content = soup.decode()
 
 def register():
     signals.content_object_init.connect(bootstrapify)
