09289ec013e9709df12d674f5685665d7391dd65
Chamon <[luizchamon] [gmail]>
2014-12-28 19:34:59 -0200

    Make compatible with PEP 8.
    Signed-off-by: Chamon <[luizchamon] [gmail]>

--- bootstrapify.py
+++ bootstrapify.py
@@ -1,15 +1,20 @@
 '''
 bootstrapify
 ===================================
-This pelican plugin modifies article and page html to use bootstrap's default classes. This is especially handy if you want to write tables in markdown, since the attr_list extension does not play nice with tables
+This pelican plugin modifies article and page html to use bootstrap's default
+classes. This is especially handy if you want to write tables in markdown,
+since the attr_list extension does not play nice with tables.
 '''
 
-from pelican import signals, contents
 from bs4 import BeautifulSoup
+from pelican import signals, contents
+
 
 def replace(searchterm, soup, attributes):
     for item in soup.select(searchterm):
-        item.attrs['class'] = list(set(item.attrs.get('class', []) + attributes))
+        item.attrs['class'] = list(set(item.attrs.get('class', []) +
+                                       attributes))
+
 
 def bootstrapify(content):
     if isinstance(content, contents.Static):
@@ -28,6 +33,7 @@ def bootstrapify(content):
         replace(selector, soup, classes)
 
     content._content = soup.decode()
+
 
 def register():
     signals.content_object_init.connect(bootstrapify)
